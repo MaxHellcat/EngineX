616f4f3b904608f5ef4a4782f30f3593821bcaab
Maxim Reshetey <maxim.reshetey@actis.ru>
2013-03-18 18:47:06 +0400

    Initial Commit

---
+++ EngineX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EngineX
+//
+//  Created by Maxim Reshetey on 18.03.13.
+//  Copyright (c) 2013 Maxim Reshetey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EngineX/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  EngineX
+//
+//  Created by Maxim Reshetey on 18.03.13.
+//  Copyright (c) 2013 Maxim Reshetey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
